440ebaea4621e792d5f274fb382330f05e7f7a51
Marllon Lima <coder.marllon@gmail.com>
2025-12-03 20:25:11 -0300

    feat() fazendo a views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+  queryset = Category.objects.all()
+  serializer_class = CategorySerializer
+  
+  
+class ProductViewSet(viewsets.ModelViewSet):
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializer 
 
 # Create your views here.
